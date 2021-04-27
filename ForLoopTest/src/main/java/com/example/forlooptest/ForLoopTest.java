08405d6ce4dd371669edeeff9464b68330ed3e7c
JIAWEI68 <ang.jiawei56@gmail.com>
2021-04-27 19:43:21 +0800

    For Loop Test V1

---
+++ ForLoopTest/src/main/java/com/example/forlooptest/ForLoopTest.java
@@ -0,0 +1,18 @@
+package com.example.forlooptest;
+
+import java.util.Scanner;
+
+public class ForLoopTest {
+    private static String input;
+
+    public static void main(String[] args) {
+
+        for (int i = 15; i <30 ; i++) {
+            System.out.println("i is: " + i);
+
+
+        }
+            
+
+    }
+}
---
+++ ForLoopTest/src/main/java/com/example/forlooptest/MyClass.java
@@ -0,0 +1,4 @@
+package com.example.forlooptest;
+
+public class MyClass {
+}
